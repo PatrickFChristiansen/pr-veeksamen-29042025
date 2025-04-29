12451119c415584e8b8c51ecb699b293a87d285f
PatrickFChristiansen <patrickfriis@hotmail.com>
2025-04-29 11:49:47 +0200

    Add files via upload

---
+++ prøveeksamen 29042025/prøveeksamen 29042025/Album.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prøveeksamen_29042025
+
+//Opgave 3.2 Klasser (60 min)
+
+// isbn, udgivelsesår, titel, tegnerNavn og antalProduceret.
+{
+    internal class Album
+    {
+        public string Isbn { get; set; }
+        public int Udgivelsesår { get; set; }
+        public string Titel { get; set; }
+        public string TegnerNavn { get; set; }
+        public int AntalProduceret { get; set; }
+        public Album(string isbn, int udgivelsesår, string titel, string tegnerNavn, int antalProduceret)
+        {
+            Isbn = isbn;
+            Udgivelsesår = udgivelsesår;
+            Titel = titel;
+            TegnerNavn = tegnerNavn;
+            AntalProduceret = antalProduceret;
+        }
+    }
+}
---
+++ prøveeksamen 29042025/prøveeksamen 29042025/Program.cs
@@ -0,0 +1,71 @@
+﻿//Prøve eksamen 29042025 Patrick Christiansen
+
+
+
+// Opret en liste af Album-objekter
+
+
+using prøveeksamen_29042025;
+
+List<Album> albums = new List<Album>();
+
+
+albums.Add(new Album("9781779505910", 2021, "Superman, the man of steal", "John Byrne", 10000));
+albums.Add(new Album("8756242190", 1989, "Tintin og alfa-kunsten", "Hergés", 12000));
+albums.Add(new Album("9780785124986", 2007, "Spider-Man: Birth of Venom", "Todd McFarlane", 8000));
+
+
+//udskriver alle album
+foreach (var album in albums)
+{
+    Console.WriteLine($"Titel: {album.Titel}, Tegner: {album.TegnerNavn}, År: {album.Udgivelsesår}");
+}
+
+//finder tegneserie af Hergés før 1990
+bool found = false;
+foreach (var album in albums)
+{
+    if (album.Udgivelsesår < 1990 && album.TegnerNavn == "Hergés")
+    {
+        Console.WriteLine("Titel: " + album.Titel);
+        Console.WriteLine("Tegner: " + album.TegnerNavn);
+        Console.WriteLine("Udgivelsesår: " + album.Udgivelsesår);
+        Console.WriteLine("ISBN: " + album.Isbn);
+        Console.WriteLine("Antal produceret: " + album.AntalProduceret);
+        Console.WriteLine("-----------------------------");
+        found = true;
+    }
+    if (!found)
+    {
+        Console.WriteLine("*** Ikke fundet ***");
+    }
+}
+
+
+
+//Opgave 3.3
+// opgave 3.3.1
+
+//forbobler tal og udskriver det 5 gange
+int tal = 2;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(tal);
+    tal = tal * 2;
+
+}
+
+
+// opgave 3.3.2
+
+// Opgave 3.3.2
+
+//udskriver ulige tal fra -10 til -20
+
+for (int tallet = -11; tallet > -20; tallet -= 2)
+{
+    Console.WriteLine(tallet);
+}
+
+
